73c04427527c4f197999ba9de3dd98342d34140f
yu-miz <gt.yumiz@gmail.com>
2017-04-19 13:59:45 +0900

    ポートフォリオサイトの素体

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $(".about").click(function () {
+        $("#wrapper").css('background-image', 'url(../img/bg2.png)');
+    });
+    $(".skill").click(function () {
+        $("#wrapper").css('background-image', 'url(../img/bg3.png)');
+    });
+    $(".work").click(function () {
+        $("#wrapper").css('background-image', 'url(../img/bg4.png)');
+    });
+    $(".contact").click(function () {
+        $("#wrapper").css('background-image', 'url(../img/bg5.png)');
+    });
+});
